8599033568f00774975c0f351690a7568bd4d626
Zudra <murilo.klug@gmail.com>
2023-12-18 17:23:03 -0300

    Começo do devbook

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios(
     id int auto_increment primary key,
-    nome varchar(50) not null,
+    nome varchar(50) not null check (nome != ''),
     nick varchar(50) not null unique,
     email varchar(50) not null unique,
     senha varchar(20) not null unique,
--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,10 +1,40 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 // CriarUsuario cria os usuários no banco
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando Usuário!"))
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarioID, erro := repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id inserido: %d", usuarioID)))
 }
 
 // BuscarUsuarios fará a busca de todos os usuários no banco
